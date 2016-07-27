f2fb16f84868867eddc1f1b093bd7c9634766061
perkzhang <perkzhang@outlook.com>
2016-07-27 08:56:46 +0800

    fix

---
+++ app/src/main/java/com/example/perk/guidetest0702/valueclass/Info.java
@@ -0,0 +1,71 @@
+package com.example.perk.guidetest0702.valueclass;
+
+/**
+ * Created by perkzhang on 2016/7/7.
+ */
+public class Info {
+    public String mName;
+    public String mNumber;
+    public String mLocation;
+    public String mLockNumber;
+    public String mPhotoPath;
+
+    public String mLatitude;//纬度
+    public String mLongitude;//经度
+
+    public String getmLatitude() {
+        return mLatitude;
+    }
+
+    public void setmLatitude(String mLatitude) {
+        this.mLatitude = mLatitude;
+    }
+
+    public String getmLongitude() {
+        return mLongitude;
+    }
+
+    public void setmLongitude(String mLongitude) {
+        this.mLongitude = mLongitude;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmNumber() {
+        return mNumber;
+    }
+
+    public void setmNumber(String mNumber) {
+        this.mNumber = mNumber;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public String getmLockNumber() {
+        return mLockNumber;
+    }
+
+    public void setmLockNumber(String mLockNumber) {
+        this.mLockNumber = mLockNumber;
+    }
+
+    public String getmPhotoPath() {
+        return mPhotoPath;
+    }
+
+    public void setmPhotoPath(String mPhotoPath) {
+        this.mPhotoPath = mPhotoPath;
+    }
+}
